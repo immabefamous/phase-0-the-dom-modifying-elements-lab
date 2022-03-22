735c63275efc72c735887bb9f67073a78be77a2b
Emmanuel <emmanuelbboatteng@yahoo.com>
2022-03-22 13:28:07 -0400

    commit tree e82e0dea2a1e7591fe14855f35356dc5ca0f9694

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector('main');
+const newHeader = document.getElementById
+newHeader.nodeName = 'H1'
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Emmanuel is the champion';
